db44b3c76da01aaff23ad62df46fcf3f7d8c1ebd
Brian Ghigiarelli <brianghig@gmail.com>
2013-04-28 18:39:56 -0400

    Initial commit

---
+++ src/com/bg2/bg2yamba/StatusActivity.java
@@ -0,0 +1,14 @@
+package com.bg2.bg2yamba;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class StatusActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+	}
+
+}
