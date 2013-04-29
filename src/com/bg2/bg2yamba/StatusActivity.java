e12631b2ee24b5ea289075e3d1058ed11d44fec5
Brian Ghigiarelli <brianghig@gmail.com>
2013-04-28 20:36:21 -0400

    adding jtwitter library with signpost in order to post to the marakana twitter api

--- src/com/bg2/bg2yamba/StatusActivity.java
+++ src/com/bg2/bg2yamba/StatusActivity.java
@@ -1,14 +1,24 @@
 package com.bg2.bg2yamba;
 
+import winterwell.jtwitter.Twitter;
 import android.os.Bundle;
 import android.app.Activity;
 
 public class StatusActivity extends Activity {
 
+	/**
+	 * Twitter client for Learning Android Marakana API
+	 */
+	protected Twitter twitter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
+		
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+		
 	}
 
 }
