5ca8e8be61985bf062d0c756e77bd09521c0221f
Brian Ghigiarelli <brianghig@gmail.com>
2013-05-06 19:08:59 -0400

    Adding barebones UpdaterService that is started/stopped via context menu in preparation for retrieving status updates from Twitter API

--- src/com/bg2/bg2yamba/StatusActivity.java
+++ src/com/bg2/bg2yamba/StatusActivity.java
@@ -72,6 +72,12 @@ public class StatusActivity extends Activity implements OnClickListener, TextWat
 			case R.id.itemPrefs:
 				startActivity(new Intent(this, PrefsActivity.class));
 				break;
+			case R.id.itemServiceStart:
+				startService(new Intent(this, UpdaterService.class));
+				break;
+			case R.id.itemServiceStop:
+				stopService(new Intent(this, UpdaterService.class));
+				break;
 		}
 		
 		return true;
