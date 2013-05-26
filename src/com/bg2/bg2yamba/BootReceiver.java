3e5de7c08e7a9212989c93a5e070f8c600883335
Brian Ghigiarelli <brianghig@gmail.com>
2013-05-26 19:07:42 -0400

    Starting the UpdaterService on system boot

---
+++ src/com/bg2/bg2yamba/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.bg2.bg2yamba;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+
+	private static final String TAG = BootReceiver.class.getSimpleName();
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		context.startService(new Intent(context, UpdaterService.class));
+		Log.d(TAG, "Boot receiver started UpdaterService");
+	}
+
+}
